a75f114e977cfeca1b760a932e70a196c029e94c
studiooriley <studiooriley@gmail.com>
2024-01-03 20:23:32 -0800

    PDF Button Works!!!!!!!!!!!!!!

--- scripts.js
+++ scripts.js
@@ -12,33 +12,3 @@ saveButton.addEventListener('click', () => {
   // Or force download
   doc.save('screenplay.pdf');
 })
-
-
-
-
-/*
-
-const saveButton = document.getElementById("save-button");
-const screenplayText = document.getElementById("screenplay-text");
-
-saveButton.addEventListener("click", () => {
-  const textToSave = screenplayText.value;
-
-  // Create a blob object representing the PDF
-  const blob = new Blob([textToSave], { type: "application/pdf" });
-
-  // Create a URL for the blob object
-  const url = URL.createObjectURL(blob);
-
-  // Create a link element and simulate a click to trigger download
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = "screenplay.pdf"; // Suggest a filename
-  link.click();
-
-  // Revoke the object URL after saving
-  URL.revokeObjectURL(url);
-});
-
-
-*/
